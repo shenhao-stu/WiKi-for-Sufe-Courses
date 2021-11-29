13fbc2f4d54a9617458e2deb791bad8883c5097b
shenhao-stu <56550322@163.com>
2021-11-29 15:29:46 +0800

    add giscus app

--- update.py
+++ update.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import os
 from urllib.parse import quote
 
-EXCLUDE_DIRS = ['.git', 'docs', '.vscode', '.circleci', 'site', 'overrides', '.github']
+EXCLUDE_DIRS = ['.git', 'docs', '.vscode',
+                '.circleci', 'site', 'overrides', '.github']
 README_MD = ['README.md', 'readme.md', 'index.md']
 
 TXT_EXTS = ['md', 'txt']
@@ -34,39 +32,28 @@ def list_files(course: str):
     return filelist_texts, readme_path
 
 
-def generate_md(course: str, filelist_texts: str, readme_path: str, topic: str):
-    final_texts = ['\n\n'.encode(), filelist_texts.encode()]
+def generate_md(course: str, filelist_texts: str, readme_path: str):
+    final_texts = ['\n\n', filelist_texts]
     if readme_path:
-        with open(readme_path, 'rb') as file:
+        with open(readme_path, 'r', encoding='utf-8') as file:
             final_texts = file.readlines() + final_texts
-    topic_path = os.path.join('docs', topic)
-    if not os.path.isdir(topic_path):
-        os.mkdir(topic_path)
-    with open(os.path.join(topic_path, '{}.md'.format(course)), 'wb') as file:
+    with open('docs/{}.md'.format(course), 'w', encoding='utf-8') as file:
         file.writelines(final_texts)
-        
 
 
 if __name__ == '__main__':
     if not os.path.isdir('docs'):
         os.mkdir('docs')
 
-    topics = list(filter(lambda x: os.path.isdir(x) and (
-        x not in EXCLUDE_DIRS), os.listdir('.')))  # list topics
+    courses = list(filter(lambda x: os.path.isdir(x) and (
+        x not in EXCLUDE_DIRS), os.listdir('.')))  # list courses
 
-    for topic in topics:
-        topic_path = os.path.join('.', topic)
+    for course in courses:
+        filelist_texts, readme_path = list_files(course)
+        generate_md(course, filelist_texts, readme_path)
 
-        courses = list(filter(lambda x: os.path.isdir(os.path.join(topic_path, x)) and (
-            x not in EXCLUDE_DIRS), os.listdir(topic_path)))  # list courses
-
-        for course in courses:
-            course_path = os.path.join(".", topic, course)
-            filelist_texts, readme_path = list_files(course_path)
-            generate_md(course, filelist_texts, readme_path, topic)
-
-    with open('README.md', 'rb') as file:
+    with open('README.md', 'r',encoding='utf-8') as file:
         mainreadme_lines = file.readlines()
 
-    with open('docs/index.md', 'wb') as file:
+    with open('docs/index.md', 'w', encoding='utf-8') as file:
         file.writelines(mainreadme_lines)
